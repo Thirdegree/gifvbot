cc8d18e1003b375779ea6653899aae7421f8576c
Joel Christiansen <greenbadge.jc@gmail.com>
2016-03-09 14:36:56 -0600

    subs XOR coms
    	new file:   main.py
    
    oops
    
     Date:      Wed Mar 9 14:36:56 2016 -0600

---
+++ main.py
@@ -0,0 +1,31 @@
+import obot, time, praw, re
+from collections import deque
+r = obot.login()
+done = deque(maxlen=300)
+cDone = deque(maxlen=300)
+
+doComs = True
+
+def main():
+    stream = praw.helpers.submission_stream(r, "thirdegree", limit=None)
+    cStream = praw.helpers.comment_stream(r, "thirdegree", limit=None)
+
+    for sub, com in zip(stream, cStream):
+        try:
+            match = re.findall("imgur\.com\/([A-Za-z0-9]+)(?:\.gif|\.jpg)*$", sub.url)
+            cMatch= re.findall("imgur\.com\/([A-Za-z0-9]+)(?:\.gif|\.jpg)*$", com.body)
+            if match and sub not in done and not doComs:
+                sub.add_comment("http://i.imgur.com/" + match[0] + ".gifv")
+                done.append(sub)
+            if cMatch and com not in cDone and doComs:
+                com.reply("http://i.imgur.com/" + cMatch[0] + ".gifv")
+                cDone.appen(com)
+        except praw.errors.APIException:
+            pass
+
+
+if __name__ == "__main__":
+    while True:
+        main()
+        time.sleep(3)
+
